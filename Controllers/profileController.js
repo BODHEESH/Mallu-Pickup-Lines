6b74d24c8c6eea5f87d6eea7c76463927573d397
RameesRoshanck <ckmhdroshan@gmail.com>
2023-03-11 21:05:18 +0530

    follow and unfollow

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -5,9 +5,6 @@ const bcrypt = require('bcrypt');
 
 // update user profile function
 const updateProfile=async(req,res)=>{
-    console.log(req.user,'=========');
-      console.log(req.params.id,'++++++++++');
-      console.log(req.body,'req.body');
       try{
         const { _id} = req.user
         let {username,mobileNumber,password,profilePictureName}=req.body
@@ -23,7 +20,6 @@ const updateProfile=async(req,res)=>{
                         profilePicture:profilePictureName
                     }
                 })
-                console.log('sucess',updateProfile);
                res.status(200).json({message:'suceesfully updated',updateProfile})
         }else{
             res.status(401).json({message:"unauthorized access"})
@@ -35,8 +31,76 @@ const updateProfile=async(req,res)=>{
 }
 
 
+//follow user
+const followUser=async(req,res)=>{
+    try{
+        const { _id} = req.user
+        let id=req.params.id
+        if(_id==id){
+            let userId=req.body.userId
+            if(id!==userId){
+                 let user=await User.findOne({_id:id})
+                 let currentUser=await User.findOne({_id:userId})
+                 if(!user.followers.includes(userId)){
+                    await user.updateOne({$push:{followers:userId}})
+                    await currentUser.updateOne({$push:{followings:id}})
+                    return res.status(200).json({ message: "user has been followed" });
+                 }else{
+                    return res
+                    .status(403)
+                    .json({ message: "you already follow this user" });
+                 }
+            }else{
+                return res.status(403).json({ message: "you can't follow yourself" });
+            }
+            
+        }else{
+            res.status(401).json({message:"unauthorized access"})
+        }
+
+    }catch(error){
+        console.log(error,'follow user');
+    }
+}
+
+const unFollowUser=async(req,res)=>{
+    console.log(req.user,'=========');
+    console.log(req.params.id,'++++++++++');
+    console.log(req.body,'req.body');
+    try{
+        const { _id} = req.user
+        let id=req.params.id
+        if(_id==id){
+            let userId=req.body.userId
+            if(id!==userId){
+                 let user=await User.findOne({_id:id})
+                 let currentUser=await User.findOne({_id:userId})
+                 if(user.followers.includes(userId)){
+                    await user.updateOne({$pull:{followers:userId}})
+                    await currentUser.updateOne({$pull:{followings:id}})
+                    console.log('success');
+                    return res.status(200).json({ message: "user has been unFollowed" });
+                 }else{
+                    return res
+                    .status(403)
+                    .json({ message: "you already unfollow this user" });
+                 }
+            }else{
+                return res.status(403).json({ message: "you can't follow yourself" });
+            }
+            
+        }else{
+            res.status(401).json({message:"unauthorized access"})
+        }
+
+    }catch(error){
+        console.log(error,'follow user');
+    }
+}
 
 
 module.exports={
     updateProfile,
+    followUser,
+    unFollowUser
 }
--- Routes/profileRoute.js
+++ Routes/profileRoute.js
@@ -1,11 +1,16 @@
 const express=require('express')
-const { updateProfile } = require('../Controllers/profileController')
+const { updateProfile, followUser, unFollowUser } = require('../Controllers/profileController')
 const router=express.Router()
 const { verifyJwt } = require("../middlewares/verifyJWT")
 
 //update user profile router
 router.post('/:id',verifyJwt,updateProfile)
 
+//follow user router
+router.put('/followUser/:id',verifyJwt,followUser)
+
+//unfollow user router
+router.put("/unfollowUser/:id",verifyJwt,unFollowUser)
 
 
 module.exports=router
